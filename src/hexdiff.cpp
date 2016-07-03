b45e6e0fc7c21d66debb4b19715832d2e7a5d8c7
Curtis <curtis99877@gmail.com>
2016-07-03 13:10:48 -0400

    GCS solution

--- src/hexdiff.cpp
+++ src/hexdiff.cpp
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <argp.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 #include "hexdiff.h"
 
@@ -16,11 +21,12 @@
  *                                                *
  **************************************************/
 
+using namespace std;
+
 /*
  *  Main function
  */
 int main(int argc, char** argv) {
-  FILE *f1, *f2;
   struct arguments arguments;
 
   arguments.quiet = 0;
@@ -29,11 +35,76 @@ int main(int argc, char** argv) {
 
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-  f1 = fopen(arguments.arg1, "r");
-  f2 = fopen(arguments.arg2, "r");
+  ifstream f1, f2;
+  f1.open(arguments.arg1, ios::in | ios::binary);
+  f2.open(arguments.arg2, ios::in | ios::binary);
+
+  cout << "SIZE1: " << getSize(&f1) << endl;
+  cout << "SIZE2: " << getSize(&f2) << endl;
   
-  fclose(f1);
-  fclose(f2);
+  vector<vector<int> > lookup = vector<vector<int> >(getSize(&f1), vector<int>(getSize(&f2)));;
+  
+  int max = gcs(&lookup, &f1, &f2);
+  printf("MAX: %d\n", max);
+
+  for (int i = 0; i < lookup.size(); i++) {
+    for (int j = 0; j < lookup[i].size(); j++) {
+      cout << lookup[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  f1.close();
+  f2.close();
+}
+
+
+size_t getSize(ifstream *str) {
+  int pos = str->tellg();
+  str->seekg(0, str->end);
+  size_t size = str->tellg();
+  str->seekg(pos);
+  return size;
+}
+
+int gcs(vector< vector<int> > *lookup, ifstream *f1, ifstream *f2) {
+  // Get iteration case
+  int i = f1->tellg();
+  int j = f2->tellg();
+
+  // Base case
+  char c1, c2;
+  if (!f1->get(c1) || !f2->get(c2)) {
+    return 0;
+  }
+
+  // Fetch memoized results
+  assert(i < lookup->size() && j < (*lookup)[i].size());
+  if ((*lookup)[i][j] != 0) {
+    return (*lookup)[i][j];
+  }
+
+  // Match
+  if (c1 == c2) {
+    (*lookup)[i][j] = 1 + gcs(lookup, f1, f2);
+    return (*lookup)[i][j];
+  }
+
+  // Branch and return max
+  f1->clear();
+  f1->seekg(i, f1->beg);
+  f2->clear();
+  f2->seekg(j+1, f2->beg);
+  int a = gcs(lookup, f1, f2);
+
+  f1->clear();
+  f1->seekg(i+1, f1->beg);
+  f2->clear();
+  f2->seekg(j, f2->beg);
+  int b = gcs(lookup, f1, f2);
+
+  (*lookup)[i][j] = a > b ? a : b;
+  return a > b ? a : b;
 }
 
 
--- src/hexdiff.h
+++ src/hexdiff.h
@@ -4,6 +4,9 @@
 /*
  *  Header file for hexdiff.c
  */
+#include <iostream>
+#include <iomanip>
+#include <vector>
 
 #define ARG_KEY_QUIET     'q'
 #define ARG_KEY_RECURSIVE 'r'
@@ -29,5 +32,22 @@ struct arguments {
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state);
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+
+struct HexCharStruct {
+  unsigned char c;
+  HexCharStruct(unsigned char _c) : c(_c) { }
+};
+
+inline std::ostream& operator<<(std::ostream& o, const HexCharStruct& hs) {
+  return (o << std::hex << std::setw(2) << (int)hs.c << std::setfill('0'));
+}
+
+inline HexCharStruct hex(unsigned char _c) {
+  return HexCharStruct(_c);
+}
+
+size_t getSize(std::ifstream *str);
+
+int gcs(std::vector< std::vector<int> > *lookup, std::ifstream *f1, std::ifstream *f2);
 
 #endif
